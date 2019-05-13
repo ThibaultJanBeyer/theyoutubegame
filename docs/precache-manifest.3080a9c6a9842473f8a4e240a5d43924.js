1ab6abaf132277111da65f60fa8fa37cca5e2c79
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2019-05-13 19:53:58 +0200

    add docks postbuild step

---
+++ docs/precache-manifest.3080a9c6a9842473f8a4e240a5d43924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31eb47a7019cbd8cba341cd1e6ae3b87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "600d690806c4a17097fb",
+    "url": "/static/css/main.6e187acd.chunk.css"
+  },
+  {
+    "revision": "1ef19f6cad8dab94a0c3",
+    "url": "/static/js/2.7776b967.chunk.js"
+  },
+  {
+    "revision": "600d690806c4a17097fb",
+    "url": "/static/js/main.aa5e5cbe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "af315fbbeb5c8c05b81182be4d9e8b08",
+    "url": "/static/media/logo_fill.af315fbb.svg"
+  }
+]);
